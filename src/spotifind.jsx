c076a3292b5df26ec75160ed0cd6fba593384cf3
unknown <atj2199@gmail.com>
2024-11-15 16:08:17 +0530

    deploy14

--- src/spotifind.jsx
+++ src/spotifind.jsx
@@ -275,7 +275,214 @@ const SpotifyAuthApp = () => {
         }
       };
       const response = await axios.post('https://b768-18-213-200-192.ngrok-free.app/compare-tracks', {
-        formattedData,
+    "user1": {
+      "tracks": [
+        {
+          "id": "track1_id",
+          "name": "Track 1",
+          "artists": ["Artist 1"],
+          "features": {
+            "duration_ms": 207960,
+            "danceability": 0.696,
+            "energy": 0.905,
+            "key": 2,
+            "loudness": -2.743,
+            "mode": 1,
+            "speechiness": 0.103,
+            "acousticness": 0.011,
+            "instrumentalness": 0.000905,
+            "liveness": 0.302,
+            "valence": 0.625,
+            "tempo": 114.944,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track2_id",
+          "name": "Track 2",
+          "artists": ["Artist 2"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track3_id",
+          "name": "Track 3",
+          "artists": ["Artist 3"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track4_id",
+          "name": "Track 4",
+          "artists": ["Artist 4"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track5_id",
+          "name": "Track 5",
+          "artists": ["Artist 5"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        }
+      ]
+    },
+    "user2": {
+      "tracks": [
+        {
+          "id": "track6_id",
+          "name": "Track 6",
+          "artists": ["Artist 6"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track7_id",
+          "name": "Track 7",
+          "artists": ["Artist 7"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track8_id",
+          "name": "Track 8",
+          "artists": ["Artist 8"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track9_id",
+          "name": "Track 9",
+          "artists": ["Artist 9"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        },
+        {
+          "id": "track10_id",
+          "name": "Track 10",
+          "artists": ["Artist 10"],
+          "features": {
+            "duration_ms": 0,
+            "danceability": 0,
+            "energy": 0,
+            "key": 0,
+            "loudness": 0,
+            "mode": 0,
+            "speechiness": 0,
+            "acousticness": 0,
+            "instrumentalness": 0,
+            "liveness": 0,
+            "valence": 0,
+            "tempo": 0,
+            "time_signature": 4
+          }
+        }
+      ]
+    }
       }, {
         headers: {
           'Access-Control-Allow-Origin': '*',
