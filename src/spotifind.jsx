82de674a1586d66788ee5345812500569f4c7373
unknown <atj2199@gmail.com>
2024-11-15 16:27:01 +0530

    deploy17

--- src/spotifind.jsx
+++ src/spotifind.jsx
@@ -280,7 +280,7 @@ const SpotifyAuthApp = () => {
       const formattedString = JSON.stringify(formattedData, null, 2);
       const finalData = JSON.parse(formattedString);
   
-      const response = await axios.post('https://ce82-18-213-200-192.ngrok-free.app/compare-tracks', 
+      const response = await axios.post('https://c0f9-18-213-200-192.ngrok-free.app/compare-tracks', 
         finalData,
         {
           headers: {
